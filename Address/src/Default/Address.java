82f21401f39dcb2feba87b53844a59c9f2c456e0
Benjamin Platt <benjamin.platt@csuglobal.edu>
2026-02-09 16:54:16 -0700

    adding address finally

---
+++ Address/src/Default/Address.java
@@ -0,0 +1,16 @@
+package Default;
+
+public class Address {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Chrispy");
+		System.out.println("Bacon");
+		System.out.println("124 Abc St.");
+		System.out.println("Phoenix");
+		System.out.println("AZ");
+		System.out.println("85612");
+		
+	}
+
+}
